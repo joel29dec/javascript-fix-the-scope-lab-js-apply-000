67dcda04ee5b7ff26b1374b1c6e6a0ddbf43369f
Joel S <joel29dec@gmail.com>
2019-03-16 20:46:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+
+/*function outerFunction() {
+  var innerVariable = "I'm sort of a secret.";
+ 
+  return function innerScope() {
+    var inaccessible = "Nothing can touch me.";
+ 
+    return innerVariable;
+  }
+}
+var myScope = outerFunction();
+console.log(myScope())
+*/
+
+
 var animal = 'dog'
 
 function myAnimal() {
@@ -8,6 +23,7 @@ function yourAnimal() {
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
+  animal ='cat'
   return animal
 }
 
